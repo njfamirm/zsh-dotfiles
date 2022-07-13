8129c545ffe73b298e31a9b4e998e382a292f3af
Amir Najafi <njfamirm@gmail.com>
2022-07-13 23:01:00 +0430

    chore: merge install-deps into install script

--- install.sh
+++ install.sh
@@ -13,5 +13,10 @@ fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
 
-chmod +x ~/.dotfiles/install-deps.sh
-bash ~/.dotfiles/install-deps.sh
+# Install packages with apt
+if type apt > /dev/null; then
+   sudo apt-get -y install tmux vim git
+fi
+
+# Install oh-my-zsh (https://github.com/ohmyzsh/ohmyzsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
