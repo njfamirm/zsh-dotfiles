278c6e6ba46ebe0943328f6d9faebc0e7a7cf90e
Amir Najafi <njfamirm@gmail.com>
2022-07-13 14:09:28 +0000

    feat: add install dependencies script

---
+++ .dotfiles/install-deps.sh
@@ -0,0 +1,7 @@
+# Install packages with apt
+if type apt > /dev/null; then
+   sudo apt install -y tmux vim git
+fi
+
+# Install oh-my-zsh (https://github.com/ohmyzsh/ohmyzsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
