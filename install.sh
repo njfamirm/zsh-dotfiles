7b1d0e419650f00bfa80571f9bbee9779324ee6f
S. Amir Najafi <njfamirm@gmail.com>
2022-07-14 08:30:30 +0000

    fix: change install script

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-git clone --bare https://github.com/njfamirm/dotfiles.git $HOME/.dotfiles
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .dotfiles-backup
-dotfiles checkout 2>/dev/null
+dotfiles checkout --force
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
-dotfiles checkout
 dotfiles config status.showUntrackedFiles no
 
 # Install packages with apt
