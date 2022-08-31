0a0b8efa15f7721c67fea5c2c08d53e119ac6e6b
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-08-31 15:50:31 +0430

    feat: add apk and brew package manager support

--- install.sh
+++ install.sh
@@ -12,13 +12,26 @@ else
 fi;
 dtf config status.showUntrackedFiles no
 
+PACKAGES="tmux tree vim git zsh"
+
 # Install packages with apt
-if type apt-get > /dev/null; then
-   sudo apt-get update
-   sudo apt-get -y install tmux vim git
+if command -v apt-get >/dev/null 2>&1; then
+  sudo apt-get update
+  sudo apt-get upgrade
+  sudo apt-get -y install $PACKAGES
+elif command -v brew >/dev/null 2>&1; then
+  brew update
+  brew upgrade
+  brew install $PACKAGES
+elif command -v apk >/dev/null 2>&1; then
+  sudo apk update
+  sudo apk upgrade
+  sudo apk add $PACKAGES
+else
+  echo "Cannot find supported package manager for install $PACKAGES"
 fi
 
 # Install oh-my-zsh (https://github.com/ohmyzsh/ohmyzsh)
 if [ ! -d ~/.oh-my-zsh ];then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  git clone https://github.com/ohmyzsh/ohmyzsh .oh-my-zsh
 fi
