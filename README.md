12ff89c3165e707be3dd153b0a139ce298e36467
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-08-31 17:22:22 +0430

    feat: install package before clone repo

--- README.md
+++ README.md
@@ -2,17 +2,10 @@
 
 my dotfiles configuration.
 
-## Usage 
-
-### Install Dependencies
-- curl
-- git
-- sudo 
-
-### Install dotfiles
+## Install
 
 > ‼️ back up from your own dotfiles before install this dotfiles !!!!
 
 ```sh
-curl -fsSL https://raw.githubusercontent.com/njfamirm/dotfiles/main/install.sh | sh
+curl -fsSL https://raw.githubusercontent.com/njfamirm/dotfiles/main/install.sh | bash
 ```
--- install.sh
+++ install.sh
@@ -3,37 +3,26 @@ set -Eeuo pipefail
 trap "echo '❌ Error'" ERR
 
 echoStep () {
-  echo "📍 $1"
+  echo "📍 $1..."
 }
 
-echoStep 'Clone dotfile repo...'
-git clone --bare --recurse-submodules --jobs 8 https://github.com/njfamirm/dotfiles ~/.dotfiles
-function dtf {
-  git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+echoErr () {
+  echo "❌ $1!"
 }
 
-echoStep 'Checkout files...'
-dtf checkout --force
-
-dtf config status.showUntrackedFiles no
-
-echoStep "Clone Submodule..."
-dtf submodule init
-dtf submodule update
-
-if [ ! -d ~/.oh-my-zsh >/dev/null 2>&1 ];then
-  echoStep "Clone oh-my-zsh..."
-  git clone https://github.com/ohmyzsh/ohmyzsh .oh-my-zsh
+if command -v apt-get >/dev/null 2>&1; then
+  echoErr "sudo command not found"
+  return
 fi
 
 if [[ $OSTYPE == 'darwin'* ]]; then
-  echoStep "Install home brew..."
+  echoStep "Install home brew"
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 fi
 
-PACKAGES="tmux tree vim zsh"
+PACKAGES="tmux tree vim zsh git"
 
-echoStep "Install packages with package manager...."
+echoStep "Install packages with package manager"
 if command -v apt-get >/dev/null 2>&1; then
   sudo apt-get update
   sudo apt-get upgrade
@@ -47,6 +36,26 @@ elif command -v apk >/dev/null 2>&1; then
   sudo apk upgrade
   sudo apk add $PACKAGES
 else
-  echoErr "❌ Cannot find supported package manager for install $PACKAGES"
+  echoErr "Cannot find package manager"
+fi
+
+echoStep "Clone dotfile repo"
+git clone --bare --recurse-submodules --jobs 8 https://github.com/njfamirm/dotfiles ~/.dotfiles
+function dtf {
+  git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+echoStep "Checkout files"
+dtf checkout --force
+
+dtf config status.showUntrackedFiles no
+
+echoStep "Clone Submodule"
+dtf submodule init
+dtf submodule update
+
+if [ ! -d ~/.oh-my-zsh >/dev/null 2>&1 ];then
+  echoStep "Clone oh-my-zsh"
+  git clone https://github.com/ohmyzsh/ohmyzsh .oh-my-zsh
 fi
 
